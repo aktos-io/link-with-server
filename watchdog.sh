c56bdea99623fca3f20898abbc4b2d34b20d8f62
cca <cca@aktos1>
2017-07-15 15:19:25 +0300

    heartbeat dedection works correctly

--- watchdog.sh
+++ watchdog.sh
@@ -1,24 +1,38 @@
 #!/bin/bash
 
+port=$1
+
+
+show_usage () {
+    cat <<HELP
+
+	usage: sudo $(basename $0) PORT
+
+HELP
+    exit
+}
+
+[ $port ] || show_usage
+
 get_port_pid () {
     local port=$1
     lsof -t -i :$port
 }
 
-port=7100
 i=0
 while :; do 
   #echo "checking process for port $port"
   pid=$(get_port_pid $port)
   if [[ $pid != "" ]]; then     
     #echo "checking if port is responding...$((i++))"
-    timeout 60 ssh-keyscan -p $port localhost > /dev/null 2>&1
-    if [[ $? != 0 ]]; then 
+
+    sshd_heartbeat=$(echo | timeout 10 nc localhost $port 2> /dev/null)
+    if [[ "$sshd_heartbeat" == "" ]]; then 
       echo "can not connect to $port, killing the process (pid: $pid)"
       kill $pid
       echo "killed."
     else
-      echo "...port works OK (pid: $pid) heartbeat: $((i++))"
+      echo "...port $port works OK (pid: $pid) heartbeat: $((i++))"
     fi 
   else 
     echo "@@@ no process listening port: $port"
