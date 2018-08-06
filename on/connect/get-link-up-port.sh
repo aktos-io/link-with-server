8686953e7e926602ee145b439a7bd042d5d5e70e
Cerem Cem ASLAN <cem@aktos.io>
2018-08-06 12:27:08 +0300

    check if link up port is number

--- on/connect/get-link-up-port.sh
+++ on/connect/get-link-up-port.sh
@@ -4,7 +4,7 @@ curr_fingerprint=$(get_fingerprint $public_key)
 echo_stamp "Asking link up port for: $curr_fingerprint"
 link_up_port=$(ssh_socket_run_cmd $curr_fingerprint)
 
-if [[ $link_up_port ]]; then
+if [[ $link_up_port -eq $link_up_port ]]; then
     echo_stamp "Received link up port setting: $link_up_port"
     LINK_UP_SSHD_PORT=$link_up_port
 else
