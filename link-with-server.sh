da3c1e83187de7b8e761ad07aaa6461c81e75ff3
Cerem Cem ASLAN <cem@aktos.io>
2018-08-07 13:23:40 +0300

    simplified and cleaned up by removing port 2223, socket file etc.

--- link-with-server.sh
+++ link-with-server.sh
@@ -1,49 +1,69 @@
 #!/bin/bash
 # Author : Cerem Cem ASLAN cem@aktos.io
 # Date   : 30.05.2014
+set -u -o pipefail
+set_dir(){ _dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"; }; set_dir
+safe_source () { source $1; set_dir; }
+# end of bash boilerplate
 
-set_dir () { DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"; }; set_dir
-safe_source () { source $1 2> /dev/null; x=$?; set_dir; return $x; }
+# Strategy
+# 1. Connect to server
+# 2. Create a reverse port forward to put localhost:22 to server:$LUP
+# 3. Create a local port forward to get back server:$LUP to localhost:2222
+# 4. get host finger print of port 22 and port 2222, if they are equal, tunnel
+#    is working
 
-safe_source $DIR/config.sh || die "Required config file (./config.sh)"
+safe_source $_dir/config.sh || die "Required config file (./config.sh)"
 ORIG_LINK_UP_PORT=$LINK_UP_SSHD_PORT
 
-safe_source $DIR/aktos-bash-lib/basic-functions.sh
-safe_source $DIR/aktos-bash-lib/ssh-functions.sh
+safe_source $_dir/aktos-bash-lib/basic-functions.sh
+safe_source $_dir/aktos-bash-lib/ssh-functions.sh
 
+get_host_fingerprint(){
+    local host=$1
+    local port=${2:-22}
+    local file=$(mktemp)
+    timeout 10 ssh-keyscan -p $port $host 2> /dev/null > $file
+    ssh-keygen -l -f $file 2> /dev/null | grep ECDSA | awk '{print $2}'
+}
 
-SSH="$SSH -oStrictHostKeyChecking=no -oUserKnownHostsFile=/dev/null -oPreferredAuthentications=publickey"
+SSH="$SSH -q -oStrictHostKeyChecking=no -oUserKnownHostsFile=/dev/null \
+    -oPreferredAuthentications=publickey $SSH_USER@$SSH_HOST -p $SSH_PORT -i $SSH_KEY_FILE"
 
-on_connect_scripts_dir=$DIR/on/connect
-on_disconnect_scripts_dir=$DIR/on/disconnect
+ssh_run_cmd(){
+    $SSH $@
+}
+
+on_connect_scripts_dir=$_dir/on/connect
+on_disconnect_scripts_dir=$_dir/on/disconnect
 
 run_event_scripts () {
     local search_dir=$1
     while IFS= read -r file; do
-        echo_green $(echo_stamp "running event script: ${file#"$DIR/"}")
+        echo_green $(echo_stamp "running event script: ${file#"$_dir/"}")
         safe_source $file
     done < <(find $search_dir -type f -iname "*.sh")
 }
 
-ssh_pid=
-start_connection () {
-    # DONT USE -f OPTION, $ssh_pid is changing after a few seconds otherwise.
-    $SSH $SSH_USER@$SSH_HOST -p $SSH_PORT -i $SSH_KEY_FILE -N -M -S $SSH_SOCKET_FILE \
-        -L 2223:localhost:22 &
-    ssh_pid=$!
+link_pid=
+create_link () {
+    if [[ ! -z $link_pid ]]; then
+        echo "Killing previous link pid: $link_pid"
+        kill $link_pid 2> /dev/null
+        [[ $? -gt 0 ]] && echo "...killed already?"
+    fi
+    $SSH -N                                     \
+        -R $LINK_UP_SSHD_PORT:localhost:22      \
+        -L 2222:localhost:$LINK_UP_SSHD_PORT &  # for echo purposes
+    link_pid=$!
 }
 
-create_link () {
-    ssh_socket_make_forward -R $LINK_UP_SSHD_PORT:localhost:22 \
-        -L 2222:localhost:$LINK_UP_SSHD_PORT
-}
 
 is_port_forward_working () {
     # maybe we could try something like `ssh localhost -p 2222 exit 0`
     # in the future
-    local nc_timeout=10
-    local proxied=$(echo | timeout $nc_timeout nc localhost 2222 2> /dev/null)
-    local orig=$(echo | timeout $nc_timeout nc localhost 22 2> /dev/null)
+    local proxied=$(get_host_fingerprint 127.0.0.1 2222)
+    local orig=$(get_host_fingerprint 127.0.0.1 22)
 
     #echo "proxied: $proxied, orig: $orig"
     if [[ "$proxied" == "" ]]; then
@@ -62,33 +82,13 @@ is_port_forward_working () {
 
 shutting_down=false
 
-reconnect () {
-    start_connection
-    echo -n $(echo_stamp "starting connection (pid: $ssh_pid)")
-    for max_retry in {60..1}; do
-        if ! is_sshd_heartbeating localhost 2223; then
-            #echo_yellow "!! Broken port forward !! retry: $max_retry"
-            [[ $shutting_down == true ]] || >&2 echo -e -n "."
-        else
-            echo
-            echo_green $(echo_stamp "Connection is working")
-            shutting_down=false
-            connected=true
-            return 0
-        fi
-        sleep 1s
-    done
-    return 1
-}
-
 cleanup () {
     echo_stamp "cleaning up..."
-    if [ $ssh_pid ]; then
-        #echo "SSH pid found ($ssh_pid), killing."
-        kill $ssh_pid 2> /dev/null
+    if [ $link_pid ]; then
+        echo "SSH pid found ($link_pid), killing."
+        kill $link_pid 2> /dev/null
     fi
 }
-
 
 # ------------------------- APPLICATION --------------------------------- #
 sure_exit () {
@@ -118,28 +118,36 @@ sure_exit () {
 trap sure_exit SIGINT
 trap cleanup EXIT
 
-echo_green "using socket file: $SSH_SOCKET_FILE"
+connected=false
 while :; do
-    reconnect
     run_event_scripts $on_connect_scripts_dir
     echo_stamp "creating link 22 -> $LINK_UP_SSHD_PORT"
     create_link
-    if [ $? == 0 ]; then
-        echo_stamp "waiting for tunnel to break..."
-    else
-        echo_stamp "....unable to create a tunnel."
-    fi
+    i=0
     while :; do
         if ! is_port_forward_working; then
-            break
+            if [[ $connected == true ]]; then
+                echo_stamp "connection is broken now."
+                break
+            else
+                echo_stamp "seems not working ($i)..."
+                i=$((i + 1))
+                [[ $i -gt 5 ]] && break
+            fi
+        else
+            if [[ $connected == false ]]; then
+                echo_stamp "waiting for tunnel to break..."
+            fi
+            connected=true
+            i=0
         fi
         sleep 5
     done
+    connected=false
+    [[ $shutting_down == true ]] && sleep 2m
     echo_stamp "tunnel seems broken. cleaning up."
-    cleanup
     run_event_scripts $on_disconnect_scripts_dir
-
-    reconnect_delay=2
+    reconnect_delay=3
     echo_stamp "reconnecting in $reconnect_delay seconds..."
     shutting_down=false
     sleep $reconnect_delay
--- on/connect/get-link-up-port.sh
+++ on/connect/get-link-up-port.sh
@@ -2,9 +2,9 @@ public_key=$(get_public_key $SSH_KEY_FILE)
 curr_fingerprint=$(get_fingerprint $public_key)
 
 echo_stamp "Asking link up port for: $curr_fingerprint"
-link_up_port=$(ssh_socket_run_cmd $curr_fingerprint)
+link_up_port=$(ssh_run_cmd $curr_fingerprint)
 
-if [[ $link_up_port -eq $link_up_port ]]; then
+if [[ $link_up_port =~ ^[0-9]+$ ]] ; then # if number
     echo_stamp "Received link up port setting: $link_up_port"
     LINK_UP_SSHD_PORT=$link_up_port
 else
