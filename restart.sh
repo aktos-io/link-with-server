e42890ee69a0f894595979e6b12291f12658e39c
aktos electronics <aea@node>
2018-08-13 09:41:57 +0000

    fixed bug that prevents consequent restarts

--- restart.sh
+++ restart.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
-set -eu -o pipefail
+set -u -o pipefail
 set_dir(){ _dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"; }; set_dir
 safe_source () { source $1; set_dir; }
 # end of bash boilerplate
 
+if [[ -z ${TMUX:-} ]]; then
+    echo "Not inside a TMUX session, won't continue"
+    exit 1
+fi
+
 echo "Killing link-with-server"
-killall link-with-server.sh
+killall link-with-server.sh > /dev/null 
 sleep 2
 echo "restarting link-with-server"
-$_dir/link-with-server.sh
+exec $_dir/link-with-server.sh 
